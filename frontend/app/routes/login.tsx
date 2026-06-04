b21d9a8ee78ab2f6859931497153895fc657c7d7
jmuzy <jmuzyka4@gmail.com>
2026-06-04 13:23:09 -0400

    React router setup, tailwind added as well for easier css

---
+++ frontend/app/routes/login.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router";
+
+export default function LoginView() {
+  return (
+    <> 
+      <nav>
+        <Link to="/">Home</Link>
+        {" | "}
+        <Link to="/login">Login</Link>
+      </nav>
+      
+      <h1>Login Page Test</h1>
+    </> 
+  );
+}
+
