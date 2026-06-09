256b0d759e39c5fc15833ee48300729e2122fecd
Jason Tailor <jt598@njit.edu>
2026-06-09 18:21:53 -0400

    Imported S1-012 invalid.py into FastAPI application

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from invalid import authrouter
 app = FastAPI()
 
 origins = [
@@ -16,6 +16,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(authrouter)
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
