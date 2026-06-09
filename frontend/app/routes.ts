0cf445f0aa32b5e387d3d00b2d42465994ef8457
rc74A <158582904+rc74A@users.noreply.github.com>
2026-06-09 17:48:58 -0400

    Update routes.ts
    added a new line referencing the settings page

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, route, index } from "@react-router/dev/routes";
 export default [
 	index("routes/dashboard.tsx"),
 	route("profile", "routes/profile.tsx"),
+	route("settings", "routes/settings.tsx"),
 ] satisfies RouteConfig;
