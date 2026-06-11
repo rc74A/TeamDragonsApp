8098d3e7f0daf61ab01de7704e41d81f72650f7d
Jason Tailor <jt598@njit.edu>
2026-06-11 18:44:54 -0400

    Add layout route for authentication for S1-014

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,6 +1,10 @@
-import { type RouteConfig, route, index } from "@react-router/dev/routes";
+import { type RouteConfig, route, index, layout } from "@react-router/dev/routes";
 
 export default [
+	route("login", "welcome/login.tsx"),
+
+	layout("routes/authenticate.tsx", [
 	index("routes/dashboard.tsx"),
 	route("profile", "routes/profile.tsx"),
+	]),
 ] satisfies RouteConfig;
