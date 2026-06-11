7793ba03ecdaecd3472eea64670edebbb1f3691f
Jason Tailor <jt598@njit.edu>
2026-06-11 18:19:53 -0400

    Implement token verification in auth router
    Added token verification function to check user authentication.

--- backend/invalid.py
+++ backend/invalid.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Cookie, HTTPException, status
 
 authrouter = APIRouter(
   prefix="/api/auth",
   tags=["authenthication"]
 )
+
+def verify(token: str = Cookie(None)):
+  '''
+  Checks and verifies if the token/cookie exists for the user
+  '''
+  if not token:
+    raise HTTPException(
+      status_code=status.HTTP_401_UNAUTHORIZED,
+      detail="Access denied. Unauthenticated session state"
+    )
+  return token
 
 @authrouter.post("/logout")
 def logout(response: Response):
