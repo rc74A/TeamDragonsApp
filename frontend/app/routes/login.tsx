8bf6c8df6e5a7e8d26320e8bfc3ad043114a35a7
jmuzy <jmuzyka4@gmail.com>
2026-06-18 18:33:34 -0400

    Defined navigate

--- frontend/app/routes/login.tsx
+++ frontend/app/routes/login.tsx
@@ -7,6 +7,7 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigate = useNavigate();
 
   const handleLoginSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
